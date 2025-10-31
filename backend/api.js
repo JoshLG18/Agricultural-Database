fa653484377a15b506d6cdf289239531637c4733
JoshLG18 <joshualegrice@gmail.com>
2025-10-31 13:36:44 +0000

    fixing deployment

--- backend/api.js
+++ backend/api.js
@@ -228,7 +228,8 @@ app.delete("/:table/:id", async (req, res) => {
 
 // Start server
 connectToDb().then(() => {
-    app.listen(port, () => {
-      console.log(`Server running at http://localhost:${port}`);
-    });
+  const port = process.env.PORT || 8080;
+  app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
+  });
 });
